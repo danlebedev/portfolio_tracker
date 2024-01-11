dcc79ce3c4c3ca8af48c5c727763a8fb1344cd91
danlebedev <lebedev.py@gmail.com>
2024-01-11 16:29:22 +0300

    add in main/models.py Asset & did migration

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Asset(models.Model):
+    name = models.CharField(max_length=20)
+    short_name = models.CharField(max_length=10)
+    CHOICES = (
+        ('1', 'currency'),
+        ('2', 'cryptocurrency'),
+    )
+    asset_type = models.CharField(
+        max_length=1,
+        choices=CHOICES,
+        default='1',
+    )
